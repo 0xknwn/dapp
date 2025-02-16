a35de312b0f2c22fa8f064e0244d4e62a1fab0c6
0xknwn <145777008+0xknwn@users.noreply.github.com>
2025-02-16 20:35:54 +0100

    add vercel function

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import vercel from "vite-plugin-vercel";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), vercel()],
+  server: {
+    proxy: {
+      "/api/sepolia": {
+        target: "http://localhost:5050",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api\/sepolia/, ""),
+      },
+    },
+  },
+});
