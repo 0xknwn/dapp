8f0351df6c51de5f40c2829e413fb67dcc3d99a9
0xknwn <145777008+0xknwn@users.noreply.github.com>
2025-02-18 19:47:19 +0100

    fix build

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { use, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import WalletButton from "./wallet_button";
 import { useWallet } from "./wallet_context";
 import { num } from "starknet";
